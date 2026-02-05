77d419f893926e12c78e87ef086dc5ab58e3acf0
Devendra Mer <codeandcore2@gmail.com>
2026-02-05 20:19:22 +0530

    Header file update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -133,7 +133,7 @@ const Header = () => {
 
   return (
     <>
-    <header className={`d_flex d_flex_ac d_flex_jb ${isFixed ? "fixed" : ""}`}>
+    <header className={`d_flex d_flex_ac d_flex_jb test ${isFixed ? "fixed" : ""}`}>
       <div className="hamberger" onClick={hambergerhandle}><img src={hambergericon} alt="" /></div>
       <div className="hamberger" onClick={hambergerhandle}><img src={hambergericon} alt="" /></div>
       <nav className={`right_col ${isOpen ? 'open' : ''}`} >
